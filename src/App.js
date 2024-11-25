9685f30103e2cabd5caa09f069eb9212a4b48569
jassyio <joejassyio@gmail.com>
2024-11-25 14:16:25 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Test React App</h1>
+        <p>This is hosted on Vercel!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
